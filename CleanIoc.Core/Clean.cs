804d60b6f88f8a14c472af995d28e4492d9b7cd0
Ben Wainwright <bwainwright28@gmail.com>
2018-02-14 01:18:59 +0000

    Made it possible to set the default scan behaviour via the Clean class

--- CleanIoc.Core/Clean.cs
+++ CleanIoc.Core/Clean.cs
@@ -1,17 +1,15 @@
 ﻿namespace CleanIoc.Core
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using CleanIoc.Core.Enums;
     using CleanIoc.Core.Implementation;
 
     public static class Clean
     {
+        public static ScanBehaviour DefaultScanBehaviour { get; set; } = ScanBehaviour.ScanLoadedAssembliesForRegistries;
+
         public static IContainerBuilder MakeBuilder()
         {
-            return new ContainerBuilder();
+            return new ContainerBuilder(DefaultScanBehaviour);
         }
     }
 }
