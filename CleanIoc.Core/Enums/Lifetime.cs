b5d9c5177d09860ce0d772aee5f498305a94463e
Ben Wainwright <bwainwright28@gmail.com>
2018-02-15 21:48:52 +0000

    removed unused usings

--- CleanIoc.Core/Enums/Lifetime.cs
+++ CleanIoc.Core/Enums/Lifetime.cs
@@ -1,12 +1,5 @@
 ﻿namespace CleanIoc.Core.Enums
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-
     public enum Lifetime
     {
         None,
